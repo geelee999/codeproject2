12f1665888cfcc95ccd5d9e50407d6fe03363c5a
geelee999 <granthatcher52@gmail.com>
2017-10-11 18:46:39 -0400

    Add project files.

---
+++ codeproject2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace codeproject2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
